b054858461a50d1a116da2994fbebc3969de755f
prasanthgajula8 <prasanth080898@gmail.com>
2018-10-15 19:46:33 +0530

    Changed the prototype to infinte mode

--- Assets/_Scripts/Timer.cs
+++ Assets/_Scripts/Timer.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float time;
+    public float inittime;
+    float time;
+    float timeDecre;
     public Slider slider;
     int i;
     int j;
     int k;
+    int l;
+    int randomNumber;
     GameObject[] male;
     GameObject[] female;
     GameObject maleGoal;
@@ -20,12 +24,17 @@ public class Timer : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        time = inittime;
+        timeDecre = 0;
         _text.text = "";
         maleGoal = GameObject.FindGameObjectWithTag("MaleGoal");
         femaleGoal = GameObject.FindGameObjectWithTag("FemaleGoal");
+        male = GameObject.FindGameObjectsWithTag("Male");
+        female = GameObject.FindGameObjectsWithTag("Female");
         i = 0;
         j = 0;
         k = 0;
+        l = 0;
         slider.maxValue = 20f;
         slider.minValue = 0f;
     }
@@ -40,28 +49,38 @@ public class Timer : MonoBehaviour
             timeText.text = time.ToString();
         }
 
-        else if(time < 0)
+        else if (time < 0)
         {
-            if(i==0)
+            if (i == 0)
             {
                 checkEquity();
                 i++;
             }
         }
 
-        if(canCheck)
+        if (canCheck)
         {
-            if (j == k && (maleGoal.transform.localScale.x==femaleGoal.transform.localScale.x))
+            if (j == k && (maleGoal.transform.localScale.x == femaleGoal.transform.localScale.x))
             {
+                l++;
                 Debug.Log("Success");
-                _text.text = "Success";
+                _text.text = "Streak : " + l.ToString() ;
+                randomTeam();
+                randomGoal();
+                if (timeDecre <= 15)
+                {
+                    timeDecre += 3;
+                }
+                time = inittime - timeDecre;
+                slider.maxValue = time;
+                i = 0;
                 canCheck = false;
             }
 
             else
             {
                 Debug.Log("Failed");
-                _text.text = "Failed";
+                _text.text = "Streak : " + l.ToString() + " GameOver";
                 canCheck = false;
             }
         }
@@ -69,13 +88,10 @@ public class Timer : MonoBehaviour
 
     public void checkEquity()
     {
-        male = GameObject.FindGameObjectsWithTag("Male");
-        female = GameObject.FindGameObjectsWithTag("Female");
-
-        foreach(GameObject i in male)
+        foreach (GameObject i in male)
         {
             Color tmp = i.GetComponent<SpriteRenderer>().color;
-            if(tmp.a==1)
+            if (tmp.a == 1)
             {
                 j++;
             }
@@ -90,5 +106,102 @@ public class Timer : MonoBehaviour
             }
         }
         canCheck = true;
+    }
+
+    public void randomTeam()
+    {
+        foreach (GameObject p in male)
+        {
+            randomNumber = Random.Range(0, 100);
+
+            if (randomNumber < 50)
+            {
+                Color tmp = p.GetComponent<SpriteRenderer>().color;
+                tmp.a = 0f;
+                p.GetComponent<SpriteRenderer>().color = tmp;
+            }
+
+            else
+            {
+                Color tmp = p.GetComponent<SpriteRenderer>().color;
+                tmp.a = 1f;
+                p.GetComponent<SpriteRenderer>().color = tmp;
+            }
+        }
+
+        foreach (GameObject p in female)
+        {
+            randomNumber = Random.Range(0, 100);
+
+            if (randomNumber < 50)
+            {
+                Color tmp = p.GetComponent<SpriteRenderer>().color;
+                tmp.a = 0f;
+                p.GetComponent<SpriteRenderer>().color = tmp;
+            }
+
+            else
+            {
+                Color tmp = p.GetComponent<SpriteRenderer>().color;
+                tmp.a = 1f;
+                p.GetComponent<SpriteRenderer>().color = tmp;
+            }
+        }
+    }
+
+    public void randomGoal()
+    {
+        for(int i =0;i<2;i++)
+        {
+            if(i==0)
+            {
+                randomNumber = Random.Range(0, 100);
+
+                if (randomNumber >= 0 && randomNumber < 25)
+                {
+                    maleGoal.transform.localScale = new Vector3(8.5f, 2.87041f, 4.020913f);
+                }
+
+                else if (randomNumber >= 25 && randomNumber < 50)
+                {
+                    maleGoal.transform.localScale = new Vector3(10f, 2.87041f, 4.020913f);
+                }
+
+                else if (randomNumber >= 50 && randomNumber < 75)
+                {
+                    maleGoal.transform.localScale = new Vector3(11.5f, 2.87041f, 4.020913f);
+                }
+
+                else
+                {
+                    maleGoal.transform.localScale = new Vector3(13f, 2.87041f, 4.020913f);
+                }
+            }
+
+            else
+            {
+                randomNumber = Random.Range(0, 100);
+
+                if (randomNumber >= 0 && randomNumber < 25)
+                {
+                    femaleGoal.transform.localScale = new Vector3(8.5f, 2.87041f, 4.020913f);
+                }
+
+                else if (randomNumber >= 25 && randomNumber < 50)
+                {
+                    femaleGoal.transform.localScale = new Vector3(10f, 2.87041f, 4.020913f);
+                }
+
+                else if (randomNumber >= 50 && randomNumber < 75)
+                {
+                    femaleGoal.transform.localScale = new Vector3(11.5f, 2.87041f, 4.020913f);
+                }
+
+                else
+                {
+                    femaleGoal.transform.localScale = new Vector3(13f, 2.87041f, 4.020913f);
+                }
+            }
+        }
     }
 }
